7d097ef1cc4f847812493a315b4e13118f79d224
BrMaro <70138725+BrMaro@users.noreply.github.com>
2025-03-26 13:37:25 +0300

    add scraper configuration settings for Betika, Mozzartbet, Odibets, and SportPesa

---
+++ scrapers/config.py
@@ -0,0 +1,25 @@
+SCRAPER_SETTINGS = {
+    "betika": {
+        "url": "https://api.betika.com/v1/uo/matches",
+        "params": {
+            "page": 1,
+            "limit": 5000,
+            "tab": "upcoming",
+            "sub_type_id": "1,186,340",
+            "sport_id": 14,
+            "sort_id": 2,
+            "period_id": 9,
+            "esports": False
+        }
+    },
+    "mozzartbet": {
+        "url": "https://www.mozzartbet.co.ke/"
+    },
+    "odibets": {
+        "url": "https://www.odibets.com/"
+    },
+    "sportpesa": {
+        "url": "https://www.ke.sportpesa.com/api/highlights/1"
+    },
+    "timezone": "UTC"
+}
